c738b193d716391d77e13eb5e01fc0e2a964c817
ventu-ra <ventura09.10.0@gmail.com>
2025-07-31 14:29:36 -0300

    Add imagePath field to RestaurantDTO

--- src/main/java/dev/ventura/dto/RestaurantDTO.java
+++ src/main/java/dev/ventura/dto/RestaurantDTO.java
@@ -1,5 +1,6 @@
 package dev.ventura.dto;
 
-public record RestaurantDTO(Long id, String name, String category, double rating, String deliveryEstimate) {
+public record RestaurantDTO(Long id, String name, String category, double rating, String deliveryEstimate,
+    String imagePath) {
 
 }
