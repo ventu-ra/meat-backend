e52dce2a7454c5877e46f2364c7278fc31a8c020
ventu-ra <ventura09.10.0@gmail.com>
2025-07-31 19:33:52 -0300

    Refactor RestaurantResource to return Response objects for list and create methods

--- src/main/java/dev/ventura/resource/RestaurantResource.java
+++ src/main/java/dev/ventura/resource/RestaurantResource.java
@@ -6,13 +6,17 @@ import dev.ventura.dto.RestaurantDTO;
 import dev.ventura.entity.RestaurantEntity;
 import dev.ventura.service.RestaurantService;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/restaurants")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class RestaurantResource {
   private RestaurantService rService;
 
@@ -21,14 +25,14 @@ public class RestaurantResource {
   }
 
   @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public List<RestaurantDTO> list() {
-    return rService.listAll();
+  public Response list() {
+    return Response.ok(rService.listAll()).build();
   }
 
   @POST
   @Transactional
-  public List<RestaurantDTO> create(RestaurantEntity restaurant) {
-    return rService.create(restaurant);
+  public Response create(RestaurantEntity restaurant) {
+    rService.create(restaurant);
+    return Response.created(null).build();
   }
 }
