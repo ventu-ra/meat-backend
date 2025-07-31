ce25472f4596537a896727384b4eb58d6cd3793b
ventu-ra <ventura09.10.0@gmail.com>
2025-07-31 12:59:14 -0300

    Add method to list all restaurants as DTOs in RestaurantRepository

--- src/main/java/dev/ventura/repository/RestaurantRepository.java
+++ src/main/java/dev/ventura/repository/RestaurantRepository.java
@@ -1,10 +1,19 @@
 package dev.ventura.repository;
 
+import java.util.List;
+
+import dev.ventura.dto.RestaurantDTO;
 import dev.ventura.entity.RestaurantEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class RestaurantRepository implements PanacheRepository<RestaurantEntity> {
+
+  public List<RestaurantDTO> listAllDto() {
+    return findAll()
+        .project(RestaurantDTO.class)
+        .list();
+  }
 
 }
